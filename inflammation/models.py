7b52463cf9e5eb076ea5033ab6b5c02b71a4ebb6
harishravip <harish.raviprakash@gmail.com>
2022-02-16 10:34:42 -0500

    Assing preconditions

--- inflammation/models.py
+++ inflammation/models.py
@@ -39,9 +39,14 @@ def patient_normalise(data):
     NaN values are ignored, and normalised to 0.
     Negative values are rounded to 0.
     """
+    if not isinstance(data, np.ndarray):
+        raise TypeError('data input should be ndarray')
+    if len(data.shape) != 2:
+        raise ValueError('inflammation array should be 2-dimensional')
+    if np.any(data < 0):
+        raise ValueError('inflammation values should be non-negative')
     max_data = np.nanmax(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
         normalised = data / max_data[:, np.newaxis]
     normalised[np.isnan(normalised)] = 0
-    normalised[normalised < 0] = 0
     return normalised
