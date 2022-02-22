d96d54407b85f8bcfa3253d6cca827eb609becfc
harishravip <harish.raviprakash@gmail.com>
2022-02-22 09:50:47 -0500

    Modifications from Session 3 prior to SR1

--- inflammation/models.py
+++ inflammation/models.py
@@ -50,3 +50,71 @@ def patient_normalise(data):
         normalised = data / max_data[:, np.newaxis]
     normalised[np.isnan(normalised)] = 0
     return normalised
+
+
+class Observation:
+    def __init__(self, day, value):
+        self.day = day
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+    
+    def __str__(self):
+        return self.name
+
+
+class Patient(Person):
+    def __init__(self, name, observations=None):
+        super().__init__(name)
+        self.observations = []
+        if self.observations is not None:
+            self.observations = observations
+
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1].day + 1
+            except IndexError:
+                day = 0
+
+        new_observation = Observation(day, value)
+
+        self.observations.append(new_observation)
+        return new_observation
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def last_observation(self):
+        return self.observations[-1]
+
+
+class Doctor(Person):
+    def __init__(self, name):
+        super().__init__(name)
+        self.patients = []
+
+    def add_patient(self, patient):
+        if patient in self.patients:
+            return
+        self.patients.append(patient)
+
+
+# alice = Patient('Alice')
+# print(alice)
+#
+# obs = alice.add_observation(3)
+# print(obs)
+#
+# bob = Person('Bob')
+# print(bob)
+#
+# obs = bob.add_observation(4)
+# print(obs)
--- tests/test_patient.py
+++ tests/test_patient.py
@@ -2,9 +2,52 @@
 
 
 def test_create_patient():
+    """Check a patient is created correctly given a name."""
     from inflammation.models import Patient
 
     name = 'Alice'
     p = Patient(name=name)
 
     assert p.name == name
+
+
+def test_patient_is_person():
+    """Check if a patient is a person. """
+    from inflammation.models import Person, Patient
+    bob = Patient('Bob')
+    assert isinstance(bob, Person)
+
+
+def test_create_doctor():
+    """Check a doctor is created correctly given a name."""
+    from inflammation.models import Doctor
+    name = "Evan"
+    evan = Doctor(name)
+    assert evan.name == name
+
+
+def test_doctor_is_person():
+    """Check if a doctor is a person."""
+    from inflammation.models import Doctor, Person
+    evan = Doctor("Evan")
+    assert isinstance(evan, Person)
+
+
+def test_add_patients():
+    """Check patients are being added correctly by a doctor. """
+    from inflammation.models import Doctor, Patient
+    evan = Doctor("Evan")
+    alice = Patient("Alice")
+    evan.add_patient(alice)
+    assert evan.patients is not None
+    assert len(evan.patients) == 1
+
+
+def test_no_duplicates():
+    """Check adding the same patient to the same doctor twice does not result in duplicates. """
+    from inflammation.models import Doctor, Patient
+    doc = Doctor("Sheila Wheels")
+    alice = Patient("Alice")
+    doc.add_patient(alice)
+    doc.add_patient(alice)
+    assert len(doc.patients) == 1
